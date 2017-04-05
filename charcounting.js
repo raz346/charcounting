ef8037f536d6cb6f274d0d5710a2e366d5ce0e12
raz346 <alhashimi346@gmail.com>
2017-04-05 20:56:11 +0000

    program is done and add the js file

--- charcounting.js
+++ charcounting.js
@@ -1,1 +1,26 @@
-charcounting.js
+
+// func to count letters and return obj
+var lettersCount = {
+
+ };
+
+
+function countLetters(input) {
+  // get rid of the spaces
+  var sentence = input.replace(/\s/g, "");
+  //  go through the char of the string
+
+  for (var i = 0; i < sentence.length; i++) {
+    var currentChar = sentence[i];
+    if ( currentChar in lettersCount) {
+      // push to the object
+      lettersCount[currentChar] += 1;
+    }
+    else { lettersCount[currentChar] = 1 ;}
+  }
+
+}
+countLetters("lighthouse in the house ");
+// console.log(countLetters("lighthouse in the house "))
+console.log(lettersCount);
+
